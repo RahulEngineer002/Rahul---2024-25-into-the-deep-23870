b999105a2fc762ad6f3b73a3f3f2ad5457f76ee4
RahulEngineer002 <rahul.sankar05@gmail.com>
2024-11-28 21:16:46 -0500

    Deleting all test Comments
    I got rid of all test comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FTCRoboAvengersTimerAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/FTCRoboAvengersTimerAuton.java
@@ -60,11 +60,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-// hello
-// I am pretty cool
-// Wahoo I am awesome
-// Coding is cool
-// Let's go to Mars
 @Autonomous(name="RoboAvengers Timer Auton", group="Robot")
 //@Disabled
 public class FTCRoboAvengersTimerAuton extends LinearOpMode
